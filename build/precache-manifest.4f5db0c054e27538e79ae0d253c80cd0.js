3fffbd71f1c8d578fe0da4f662d7c9698bac5c63
Slothi <Yazan.Siyam@Outlook.com>
2020-10-07 19:50:15 -0400

    Added all changes at once, including package.json work and app src

---
+++ build/precache-manifest.4f5db0c054e27538e79ae0d253c80cd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "65e50671ab278b72c64d",
+    "url": "/static/js/main.65e50671.chunk.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "65e50671ab278b72c64d",
+    "url": "/static/css/main.15300024.chunk.css"
+  },
+  {
+    "revision": "5568a136022e677115ddc23eb6ff106e",
+    "url": "/index.html"
+  }
+];
