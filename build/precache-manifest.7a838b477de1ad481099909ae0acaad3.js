c3348ad8a7fb98defbb3d06735c478b297cb0856
junnysmiles <junyangan@hotmail.com>
2020-10-07 20:27:21 -0400

    Added eslistConfig and browserslist in package.json file, executed npm start and run.

---
+++ build/precache-manifest.7a838b477de1ad481099909ae0acaad3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d381eab4c7cd25a6dad5",
+    "url": "/static/css/main.0323221a.chunk.css"
+  },
+  {
+    "revision": "d381eab4c7cd25a6dad5",
+    "url": "/static/js/main.d381eab4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "b254632e102d81a13cd9ce6b1e9e5a9f",
+    "url": "/index.html"
+  }
+];
